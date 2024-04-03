029edcc71c7e63f66a6433970afdbb1f6c322e2d
vip440 <vipul887784@gmail.com>
2024-04-03 21:43:40 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Button } from "react-bootstrap";
+// import { Button } from "react-bootstrap";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import Login from "./Login";
+import Register from "./Register";
+import AddProduct from "./AddProduct";
+import UpdateProduct from "./UpdateProduct";
+
 function App() {
   return (
     <div className="App">
-      <h1>E-Comm Project</h1>
-      <button>Normal Button</button>
-      <Button>Bootstrap Button</Button>
+      <BrowserRouter>
+        <Header />
+        <h1>E-Comm Project</h1>
+        <Routes>
+          <Route path="/login" element={<Login />}>
+            Login
+          </Route>
+          <Route path="/register" element={<Register />}>
+            Register
+          </Route>
+          <Route path="/add" element={<AddProduct />}>
+            AddProduct
+          </Route>
+          <Route path="/update" element={<UpdateProduct />}>
+            UpdateProduct
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
